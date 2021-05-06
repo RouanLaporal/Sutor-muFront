ca1400a160727ff7e32c2805d4dc58fb22985fbe
rouan <rouan.laporal@outlook.com>
2021-05-07 01:19:30 +0200

    connexion à l'api mise en place de la connection, de l'inscription et du chat

--- asset/script/index.js
+++ asset/script/index.js
@@ -1,4 +1,9 @@
-var stream = document.getElementById('stream')
+var stream = document.getElementById('stream');
+var messageArea = document.getElementById('messageArea');
+
+
+let userName = "";
+
 stream.addEventListener('click', () => {
     fetch("http://localhost:3000")
         .then(response => {
@@ -13,4 +18,107 @@ stream.addEventListener('click', () => {
                 flvPlayer.play();
             }
         })
+})
+
+messageArea.addEventListener('focusin', ()=> {
+    var socket = io('http://localhost:3000');
+    var inboxPeople = document.querySelector(".inbox__people");
+    var messageForm = document.querySelector(".message_form");
+    var messageBox = document.querySelector(".messages__history");
+    var fallback = document.querySelector(".fallback");
+
+
+    let userName = "";
+    
+    var newUserConnected = (user) => {
+        userName = user || `User${Math.floor(Math.random() * 1000000)}`;
+        socket.emit("new user", userName);
+        addToUserBox(userName);
+    };
+    
+    var addToUserBox = (userName) => {
+        if(!!document.querySelector(`.${userName}-userlist`)){
+            return;
+        }
+        var userBox = `
+            <div class="chat_ib ${userName}-userlist">
+                <h5> ${userName} </h5>
+                </div>
+                `;
+        inboxPeople.innerHTML += userBox;
+    };
+
+    
+    var addNewMessage = ({ user, message }) => {
+        var time = new Date();
+        var formattedTime = time.toLocaleString("fr-FR",  {hour: "numeric", minute:"numeric"});
+        
+        var receivedMsg = `
+        <div class="incoming__message">
+            <div class="recerived__message">
+                <p>${message}</p>
+                <div class="message__info">
+                    <span class="message__author">${user}</span>
+                    <span class="time_data">${formattedTime}</span>
+                </div>
+            </div>
+        </div>`;
+
+        var myMsg = `
+        <div class="outgoing__message">
+            <div class="sent__message">
+                <p>${message}</p>
+                <div class="message__info">
+                    <span class="time_date">${formattedTime}</span>
+                </div>
+            </div>
+        </div> `;
+
+        messageBox.innerHTML += user === userName ? myMsg : receivedMsg;
+    };
+
+    newUserConnected();
+    
+    messageForm.addEventListener("submit", (e) =>{
+        e.preventDefault();
+        if(!messageArea.value){
+            return;
+        }
+        socket.emit("chat message", {
+            message: messageArea.value,
+            nick: userName,
+        });
+
+        messageArea.value = "";
+    });
+
+    messageArea.addEventListener("keyup",()=>{
+        socket.emit("typing",{
+            isTyping: messageArea.value.length > 0,
+            nick: userName,
+        });
+    });
+
+    socket.on("new user", function (data){
+        data.map((user) => addToUserBox(user));
+    });
+
+    socket.on("user disconnected", function(userName){
+        document.querySelector(`.${userName}-userlist`).remove();
+    });
+
+    socket.on("chat message", function(data){
+        addNewMessage({user: data.nick, message: data.message });
+    });
+
+    
+
+    socket.on("typing", function(data){
+        var { isTyping, nick } = data;
+        if(!isTyping){
+            fallback.innerHTML ="";
+            return;
+        }
+        fallback.innerHTML = `<p>${nick} is typing...</p>`;
+    });
 })
---
+++ asset/script/register.js
@@ -0,0 +1,66 @@
+var registerForm = document.getElementById("registerform");
+var z = document.getElementById("btn");
+var loginForm = document.getElementById("loginform");
+
+registerForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let username = document.getElementById("registerUsername");
+    let password = document.getElementById("registerPassword");
+    let data = {
+        username: username.value,
+        password: password.value,
+    }
+    let body = JSON.stringify(data);
+    let headers = { 'Content-Type': "application/json; charset=UTF-8", };
+
+    fetch("http://localhost:3000/users/signup", {
+        method: 'POST',
+        body: body,
+        headers: headers,
+        mode: 'cors',
+    })
+        .then(response => response.json())
+        .then(response => {
+            if (response.message)
+                document.location.href = "./login.html";
+            else
+                alert('NOPE');
+        })
+});
+
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let username = document.getElementById("loginUsername")
+    let password = document.getElementById("loginPassword")
+    let data = {
+        username: username.value,
+        password: password.value,
+    }
+    let body = JSON.stringify(data);
+    let headers = { "Content-type": "application/json; charset=UTF-8", };
+    //get access to all the data  from the api
+    fetch("http://localhost:3000/users/login", {
+        method: "POST",
+        body: body,
+        headers: headers,
+    })
+        .then(response => response.json())
+        .then(response => {
+            if (response.userId && response.token)
+                document.location.href = "./profil.html";
+            else
+                alert('NOPE');
+        })
+})
+
+function register() {
+    loginForm.style.left = "-400px";
+    registerForm.style.left = "50px";
+    z.style.left = "110px";
+}
+
+function login() {
+    loginForm.style.left = "50px";
+    registerForm.style.left = "450px";
+    z.style.left = "0px";
+}
